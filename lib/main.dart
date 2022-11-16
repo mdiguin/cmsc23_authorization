61b5dd25ab97c86cee4cda5edcdb632b3810fb9f
mdiguin <mdiguin@up.edu.ph>
2022-11-16 15:42:56 +0800

    Add sample files

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+/*
+  Created by: Claizel Coubeili Cepe
+  Date: 27 October 2022
+  Description: Sample todo app with networking
+*/
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:week7_networking_discussion/providers/todo_provider.dart';
+import 'package:week7_networking_discussion/providers/auth_provider.dart';
+import 'package:week7_networking_discussion/screens/todo_page.dart';
+import 'package:week7_networking_discussion/screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ((context) => TodoListProvider())),
+        ChangeNotifierProvider(create: ((context) => AuthProvider())),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SimpleTodo',
+      initialRoute: '/',
+      routes: {'/': (context) => const AuthWrapper()},
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (context.watch<AuthProvider>().isAuthenticated) {
+      return const TodoPage();
+    } else {
+      return const LoginPage();
+    }
+  }
+}
